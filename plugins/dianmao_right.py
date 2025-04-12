1f50e697cef9eac998f3e9e9ea5ffaef8eaeaa9e
Chenjx12 <155601432+Chenjx12@users.noreply.github.com>
2025-04-12 23:34:29 +0800

    Update dianmao_right.py

--- plugins/dianmao_right.py
+++ plugins/dianmao_right.py
@@ -26,10 +26,10 @@ async def Test1_send(bot: Bot, event: GroupMessageEvent, state: T_State):
     reply_msg = f""
  #   await Test.send(Message.reply(event.message_id) + MessageSegment.text(reply_msg))
 #    await Test.finish(MessageSegment.at(user_id))
-    db = pymysql.connect(host='localhost',
-                     user='myadmin',
-                     password='password',
-                     database='qqbot')
+    db = pymysql.connect(host='yourhost',
+                     user='youruser',
+                     password='yourpassword',
+                     database='yourdatabase')
     
     # 使用 cursor() 方法创建一个游标对象 cursor
     cursor = db.cursor()
